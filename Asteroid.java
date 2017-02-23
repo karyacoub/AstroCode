8f313fab39e940221d7b19fed0ebff2602103c61
Kareem <karyacoub@gmail.com>
2017-02-23 15:53:39 -0600

    Implemented constructor, getters, setters

--- Asteroid.java
+++ Asteroid.java
@@ -1,4 +1,21 @@
 public class Asteroid
 {
-    
+    private double size;
+    private double speed;
+
+    public Asteroid(double size, double speed)
+    {
+        this.speed = speed;
+        this.size = size;
+    }
+
+    public getSpeed()
+    {
+        return speed;
+    }
+
+    public getSize()
+    {
+        return size;
+    }
 }
