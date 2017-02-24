bb53ba7fa52b16b86b76dde95ba07405c30cd101
kareem <karyacoub@gmail.com>
2017-02-24 10:03:50 -0600

    Created Asteroid class with basic constructor, getters, setters

---
+++ src/application/Asteroid.java
@@ -0,0 +1,22 @@
+package application;
+
+public class Asteroid 
+{
+	private double size;
+	private double speed;
+	
+	public Asteroid(double size, double speed)
+	{
+		this.size = size;
+		this.speed = speed;
+	}
+	
+	public double getSize()
+	{
+		return size;
+	}
+	public double getSpeed()
+	{
+		return speed;
+	}
+}
