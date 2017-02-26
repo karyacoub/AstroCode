d36556e87e7d370443e100f059e8a084a7ee30ad
kareem <karyacoub@gmail.com>
2017-02-26 16:52:08 -0600

    Creates a single asteroid that falls towards the screen

--- src/application/Asteroid.java
+++ src/application/Asteroid.java
@@ -1,17 +1,75 @@
+/** Asteroid.java **/
+// stores asteroid data to be used in conjunction with AsteroidField class
+
 package application;
 
-public class Asteroid 
+import java.awt.Point;
+import java.util.Random;
+import javafx.scene.image.Image;
+
+public class Asteroid
 {
+	private static final int MIN_SIZE = 50;
+	private static final int MAX_SIZE = 200;
+	private static final int MIN_SPEED = 1;
+	private static final int MAX_SPEED = 5; 	// TEMPORARY //
+	
+	private Image image;
 	private double size;
 	private double speed;
+	private Point position;
 	
+	// constructor to be used if no Internet connection is found
 	public Asteroid(double size, double speed)
 	{
 		this.size = size;
 		this.speed = speed;
+		
+		int windowWidth = Main.getWidth();
+		int startingXPosition = randInt(0, windowWidth);
+		
+		position = new Point(startingXPosition, 0);
+		
+		image = new Image("file:assets/temp_asteroid.png", size, size, false, false);
 	}
 	
+	public Asteroid()
+	{
+		size = randInt(MIN_SIZE, MAX_SIZE);
+		speed = randInt(MIN_SPEED, MAX_SPEED);
 
+		int windowWidth = Main.getWidth();
+		int startingXPosition = randInt(0, windowWidth);
+		
+		position = new Point(startingXPosition, 0);
+		
+		image = new Image("file:assets/temp_asteroid.png", size, size, false, false);
+	}
+	
+	
+	public void moveTowardsShip()
+	{
+		
+	}
+	
+	private int randInt(int lowerBound, int upperBound)
+	{
+		Random rand = new Random();
+		return rand.nextInt(upperBound) + lowerBound;
+	}
+	
+	public int getX()
+	{
+		return (int) position.getX();
+	}
+	public int getY()
+	{
+		return (int) position.getY();
+	}
+	public Image getImage()
+	{
+		return image;
+	}
 	public double getSize()
 	{
 		return size;
--- src/application/Main.java
+++ src/application/Main.java
@@ -1,3 +1,6 @@
+/* Main.hava */
+// initializes main window, starts the simulation
+
 package application;
 	
 import javafx.application.Application;
@@ -6,21 +9,26 @@ import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 
 
-public class Main extends Application {
+public class Main extends Application 
+{
+	private static final int WINDOW_WIDTH = 1000;
+	private static final int WINDOW_HEIGHT = 1000;
+	private static final BorderPane root = new BorderPane();
+	
 	@Override
 	public void start(Stage primaryStage) 
 	{
-		Asteroid a = new Asteroid(20, 20); // for testing purposes
-		
 		try 
 		{
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			
-			//scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			
-			primaryStage.setScene(scene);
+			Scene tempScene = new Scene(root, WINDOW_WIDTH, WINDOW_HEIGHT);
+			primaryStage.setScene(tempScene);
 			primaryStage.show();
+			
+			AsteroidField asteroidField = new AsteroidField();
+			
+			root.getChildren().add(asteroidField);
+			
+			asteroidField.runSimulation();
 		} 
 		catch(Exception e) 
 		{
@@ -31,5 +39,19 @@ public class Main extends Application {
 	public static void main(String[] args) 
 	{
 		launch(args);
+	}
+	
+	
+	public static BorderPane getBorderPane()
+	{
+		return root;
+	}
+	public static int getWidth()
+	{
+		return WINDOW_WIDTH;
+	}
+	public static int getHeight()
+	{
+		return WINDOW_HEIGHT;
 	}
 }
