b382261868249cd3db341c01a2bfae7c3d5cf7fc
kareem <karyacoub@gmail.com>
2017-03-10 16:22:59 -0600

    added getAsteroid() method, creates asteroid based on parameter string

--- src/asteroids/AsteroidFactory.java
+++ src/asteroids/AsteroidFactory.java
@@ -2,5 +2,23 @@ package asteroids;
 
 public class AsteroidFactory 
 {
-
+	public Asteroid getAsteroid(String asteroidType)
+	{
+		asteroidType = asteroidType.toLowerCase();
+		
+		if(!asteroidType.equals("big") || !asteroidType.equals("medium") || !asteroidType.equals("small"))
+		{
+			throw new IllegalArgumentException("Revieved asteroid type does not exist");
+		}
+		else if(asteroidType.equals("big"))
+		{
+			return new BigAsteroid();
+		}
+		else if(asteroidType.equals("medium"))
+		{
+			return new MediumAsteroid();
+		}
+		
+		return new SmallAsteroid();
+	}
 }
