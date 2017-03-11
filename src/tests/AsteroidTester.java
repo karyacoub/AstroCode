bb70d64cde5af0e6993552a382281aa47ee45388
kareem <karyacoub@gmail.com>
2017-03-11 12:17:21 -0600

    tests to see whether asteroid is being correctly initialized

--- src/tests/AsteroidTester.java
+++ src/tests/AsteroidTester.java
@@ -1,11 +1,39 @@
 package tests;
+	
+import asteroids.AsteroidField;
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
 
-public class AsteroidTester {
 
+public class AsteroidTester extends Application 
+{
+	// apparently javafx stops me from running the program without all this stuff
+	// by giving me a runtime exception, so i can't test the asteroid classes 
+	// without having it all here
+	
+	@Override
+	public void start(Stage primaryStage)
+	{
+		try 
+		{
+			BorderPane root = new BorderPane();
+			Scene scene = new Scene(root,400,400);
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} 
+		catch(Exception e) 
+		{
+			e.printStackTrace();
+		}
+	}
+	
 	public static void main(String[] args) 
 	{
-		// TODO Auto-generated method stub
-
+		//launch(args);
+		AsteroidField asteroidField = new AsteroidField();
+		asteroidField.printAsteroidInfo();
+		System.exit(0);
 	}
-
 }
