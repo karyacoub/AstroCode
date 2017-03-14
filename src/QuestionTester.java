9a0d4a0b0362b0bac65ece69a9a1ecbc35d1fe72
Nive Parthiban <nive.parthiban@gmail.com>
2017-03-14 03:31:36 -0500

    Add files via upload

---
+++ src/QuestionTester.java
@@ -0,0 +1,19 @@
+package tests;
+
+import questions.Question;
+import questions.QuestionBank;
+
+public class QuestionTester
+{
+	public void main(String args[])
+	{
+		QuestionBank bank = new QuestionBank();
+		
+		Question q = new Question();
+		
+		Question curQ = bank.factory.createQuestion(q);
+		
+		System.out.println("Created sample question:");
+		System.out.println(curQ.getQuestion() + " " + curQ.getAnswer());
+	}
+}
