fd16689729576df079c7786ab8d9959a755efcbe
karyacoub <karyacoub@gmail.com>
2017-03-28 23:15:35 -0500

    added possible answers to be added to asteroids

--- src/questiontypes/Level1Question1.java
+++ src/questiontypes/Level1Question1.java
@@ -1,10 +1,15 @@
-package questions;
+package questiontypes;
+
+import questions.Question;
 
 public class Level1Question1 extends Question
 {
 	public Level1Question1()
 	{
 		question = "What is the type of a variable with the value 8?";
-		answer = "int";
+		actualAnswer = "int";
+		possibleAnswer1 = "int";
+		possibleAnswer2 = "string";
+		possibleAnswer3 = "boolean";
 	}
 }
