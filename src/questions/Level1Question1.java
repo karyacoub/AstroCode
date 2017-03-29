1f4b60b038c0d1c85f79d42a75ea7b303906f30d
karyacoub <karyacoub@gmail.com>
2017-03-28 20:27:46 -0500

    changed class name

--- src/questions/Level1Question1.java
+++ src/questions/Level1Question1.java
@@ -1,8 +1,8 @@
 package questions;
 
-public class Level1Question extends Question
+public class Level1Question1 extends Question
 {
-	Level1Question()
+	public Level1Question1()
 	{
 		question = "What is the type of a variable with the value 8?";
 		answer = "int";
