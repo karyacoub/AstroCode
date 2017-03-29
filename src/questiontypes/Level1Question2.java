b4a332b11c4533165357a8f2f7f63c73bc65c7b6
karyacoub <karyacoub@gmail.com>
2017-03-28 23:14:43 -0500

    added sample question

---
+++ src/questiontypes/Level1Question2.java
@@ -0,0 +1,15 @@
+package questiontypes;
+
+import questions.Question;
+
+public class Level1Question2 extends Question
+{
+	public Level1Question2()
+	{
+		question = "sample question";
+		actualAnswer = "sample answer";
+		possibleAnswer1 = "sample 1";
+		possibleAnswer2 = "sample 2";
+		possibleAnswer3 = "sample 3";
+	}
+}
