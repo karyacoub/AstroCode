9f92efb8f35b36c4291c18ef6a16163da0f9895d
karyacoub <karyacoub@gmail.com>
2017-04-01 14:11:04 -0500

    enter key stores input and clears text box

--- src/level/InputProcessor.java
+++ src/level/InputProcessor.java
@@ -1,13 +1,16 @@
 package level;
 
 import application.MainWindow;
+import javafx.event.EventHandler;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.HBox;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.text.Font;
 
 public class InputProcessor
 {
-    private TextField input;
+    private TextField textBox;
+    private String input;
     private String answer;
 
     /**
@@ -15,12 +18,27 @@ public class InputProcessor
      */
     public InputProcessor()
     {
-    	input = new TextField();
-    	input.setPromptText("Type in answer here...");
-    	input.setPrefHeight(150);
-    	input.setFont(Font.font("Courier New", 45));
+    	textBox = new TextField();
+    	textBox.setPromptText("Type in answer here...");
+    	textBox.setPrefHeight(150);
+    	textBox.setFont(Font.font("Courier New", 45));
+    	
+    	textBox.setOnKeyPressed(new EventHandler<KeyEvent>()
+    			{
+    				@Override
+    				public void handle(KeyEvent e)
+    				{
+    					if(e.getCode() == KeyCode.ENTER)
+    					{
+    						setInput();
+    						textBox.clear();
+    						System.out.println(input);
+    					}
+    				}
+    			}
+    	);
 
-    	MainWindow.getBorderPane().setBottom(input);
+    	MainWindow.getBorderPane().setBottom(textBox);
     }
     
     public String setAnswer(Level level)
@@ -31,17 +49,17 @@ public class InputProcessor
 
     public String setInput()
     {
-        // put your code here
-        return "dummy input"; //getInput();
+        input = textBox.getText();
+        return input;
     }
     
     public String getInput()
     {
-       return "dummy input"; //s.nextLine();  
+       return input;
     }
     
     public boolean isAnswer()
     {
-        return true;
+        return input.equals(answer);
     }
 }
