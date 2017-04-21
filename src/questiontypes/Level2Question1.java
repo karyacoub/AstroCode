27a1ddd6fb985bbe1d4e81d3b870fc8233ce797d
kareem <kareem@kareem-ThinkPad-13>
2017-04-21 10:46:54 -0500

    added sample question with sample answers

---
+++ src/questiontypes/Level2Question1.java
@@ -0,0 +1,15 @@
+package questiontypes;
+
+import questions.Question;
+
+public class Level2Question1 extends Question
+{
+	public Level2Question1()
+	{
+		question = "this is a sample question";
+		actualAnswer = "sample answer 1";
+		possibleAnswer1 = "sample answer 1";
+		possibleAnswer2 = "sample answer 2";
+		possibleAnswer3 = "sample answer 3";
+	}
+}
