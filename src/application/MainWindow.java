2c84e1fa32621a76bf2a99e53190f3edb084cea5
Nive Parthiban <nive.parthiban@gmail.com>
2017-04-25 02:06:43 -0500

    Added simple main menu. Can add more buttons if wanted

--- src/application/MainWindow.java
+++ src/application/MainWindow.java
@@ -7,6 +7,11 @@ import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
 
 
 public class MainWindow extends Application 
@@ -20,12 +25,44 @@ public class MainWindow extends Application
 	{
 		try 
 		{
-			Scene scene = new Scene(root, WINDOW_WIDTH, WINDOW_HEIGHT);
+			Button btn = new Button();
+			btn.setText("Start Game");
+	        btn.setOnAction(new EventHandler<ActionEvent>() {
+	 
+	            @Override
+	            public void handle(ActionEvent event) {
+	            	Scene scene2 = new Scene(root, WINDOW_WIDTH, WINDOW_HEIGHT);
+	            	primaryStage.setTitle("AstroCode");
+	    			primaryStage.setScene(scene2);
+	    			primaryStage.show();
+	            	GameLauncher g = new GameLauncher();
+	    			g.startGame();
+	            }
+	        });
+	        
+	        Button btn2 = new Button();
+			btn2.setText("Exit");
+			btn2.setOnAction(new EventHandler<ActionEvent>() {
+				 
+	            @Override
+	            public void handle(ActionEvent event) {
+	            	System.exit(0);
+	            }
+	        });
+	        
+	        Pane root = new Pane();
+	        btn.setLayoutX(105);
+	        btn.setLayoutY(105);
+	        root.getChildren().add(btn);
+	        
+	        btn2.setLayoutX(125);
+	        btn2.setLayoutY(145);
+	        root.getChildren().add(btn2);
+
+	        Scene scene = new Scene(root, 300, 300);
+	        primaryStage.setTitle("AstroCode");
 			primaryStage.setScene(scene);
-			primaryStage.show();
-			
-			GameLauncher g = new GameLauncher();
-			g.startGame();
+			primaryStage.show();	
 		}
 		catch(Exception e) 
 		{
